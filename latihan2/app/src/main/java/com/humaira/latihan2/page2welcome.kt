f9ca536e4d2ecfade0e91cc2e80ab019b31bc71b
humairaelfi04 <humairaelfiputri@gmail.com>
2024-08-30 13:58:36 +0700

    latihan2

---
+++ latihan2/app/src/main/java/com/humaira/latihan2/page2welcome.kt
@@ -0,0 +1,28 @@
+package com.humaira.latihan2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class page2welcome : AppCompatActivity()
+{
+
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_page2welcome)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+        { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
